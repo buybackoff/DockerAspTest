70fbe4371349147368b76091b9a2778953c658d3
buybackoff <vbaybekov@gmail.com>
2018-07-06 21:07:41 +0300

    Test EFS limits. 32768 files could be opened with ReadWrite, they do not count towards max lock of 8192 over max 256 files with simple FileStream. It uses flock. Maybe fcntl does...

--- Program.cs
+++ Program.cs
@@ -17,68 +17,104 @@ namespace DockerAspTest
     {
         public static void Main(string[] args)
         {
+            if (File.Exists($"/globaldata/error_{Process.GetCurrentProcess().Id}.txt"))
+            {
+                return;
+            }
+            var errf = File.OpenWrite($"/globaldata/error_{Process.GetCurrentProcess().Id}.txt");
+            var fileWriter = new StreamWriter(errf);
+            var count = 33000;
+            FileStream[] fss = new FileStream[count];
+            int i;
+            var sw = new Stopwatch();
+            sw.Start();
             try
             {
-                //if (Directory.Exists("/localdata/shared_lmdb"))
-                //{
-                //    Directory.Delete("/localdata/shared_lmdb", true);
-                //}
-                var env = new LMDBEnvironment("/localdata/shared_lmdb", DbEnvironmentFlags.NoSync | DbEnvironmentFlags.WriteMap);
-                env.MapSize = 300L * 1024 * 1024 * 1024;
-                env.Open();
-
-                // write 1M values and then read them
-
-                var db = env.OpenDatabase("test", new DatabaseConfig(DbFlags.Create | DbFlags.IntegerKey)).Result;
-
-                var count = 100_000_000;
-                var lastValue = 0;
-                var myupdates = 0;
-                env.Write(txn => { db.Truncate(txn); txn.Commit(); });
-
-                // Thread.Sleep(5000);
-
-                var sw = new Stopwatch();
-                sw.Start();
-
-                while (lastValue < count)
+                for (i = 0; i < count; i++)
                 {
-                    env.Write(txn =>
-                    {
-                        using (var c = db.OpenCursor(txn))
-                        {
-                            int key = default(int);
-
-                            if (c.TryGet(ref key, ref lastValue, CursorGetOption.Last))
-                            {
-                                key++;
-                                if (key - 1 == lastValue)
-                                {
-                                    myupdates++;
-                                }
-
-                                lastValue = key;
-                            }
-
-                            c.Put(ref key, ref lastValue, CursorPutOptions.NoOverwrite);
-                            txn.Commit();
-                        }
-                    });
+                    var fs = new FileStream($"/globaldata/{i}.txt", FileMode.OpenOrCreate,
+                        FileAccess.ReadWrite, FileShare.ReadWrite, 4096,
+                        FileOptions.RandomAccess);
+                    fss[i] = fs;
+                    fs.WriteByte((byte)(i % 255));
+                    fs.Flush();
                 }
-
                 sw.Stop();
-                env.Close().Wait();
-                var elapsed = sw.ElapsedMilliseconds;
-                File.WriteAllText($"/localdata/stat_{Process.GetCurrentProcess().Id}.txt", $"{myupdates} - {elapsed}");
+                File.WriteAllText($"/globaldata/ok_{Process.GetCurrentProcess().Id}.txt", sw.ElapsedMilliseconds.ToString());
             }
             catch (Exception ex)
             {
-                File.WriteAllText($"/localdata/error_{Process.GetCurrentProcess().Id}.txt", ex.ToString());
-
-                Console.WriteLine(ex);
+                sw.Stop();
+                fileWriter.WriteLine(sw.ElapsedMilliseconds + " - " + ex.ToString());
+                fileWriter.Flush();
+                errf.Flush();
+                // File.WriteAllText($"/globaldata/error_{Process.GetCurrentProcess().Id}.txt", sw.ElapsedMilliseconds + " - " + ex.ToString());
             }
 
-            // CreateWebHostBuilder(args).Build().Run();
+            //try
+            //{
+            //    //if (Directory.Exists("/localdata/shared_lmdb"))
+            //    //{
+            //    //    Directory.Delete("/localdata/shared_lmdb", true);
+            //    //}
+            //    var env = new LMDBEnvironment("/localdata/shared_lmdb", DbEnvironmentFlags.NoSync | DbEnvironmentFlags.WriteMap);
+            //    env.MapSize = 300L * 1024 * 1024 * 1024;
+            //    env.Open();
+
+            //    // write 1M values and then read them
+
+            //    var db = env.OpenDatabase("test", new DatabaseConfig(DbFlags.Create | DbFlags.IntegerKey)).Result;
+
+            //    var count = 100_000_000;
+            //    var lastValue = 0;
+            //    var myupdates = 0;
+            //    env.Write(txn => { db.Truncate(txn); txn.Commit(); });
+
+            //    // Thread.Sleep(5000);
+
+            //    var sw = new Stopwatch();
+            //    sw.Start();
+
+            //    while (lastValue < count)
+            //    {
+            //        env.Write(txn =>
+            //        {
+            //            using (var c = db.OpenCursor(txn))
+            //            {
+            //                int key = default(int);
+
+            //                if (c.TryGet(ref key, ref lastValue, CursorGetOption.Last))
+            //                {
+            //                    key++;
+            //                    if (key - 1 == lastValue)
+            //                    {
+            //                        myupdates++;
+            //                    }
+
+            //                    lastValue = key;
+            //                }
+
+            //                c.Put(ref key, ref lastValue, CursorPutOptions.NoOverwrite);
+            //                txn.Commit();
+            //            }
+            //        });
+            //    }
+
+            //    sw.Stop();
+            //    env.Close().Wait();
+            //    var elapsed = sw.ElapsedMilliseconds;
+            //    File.WriteAllText($"/localdata/stat_{Process.GetCurrentProcess().Id}.txt", $"{myupdates} - {elapsed}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    File.WriteAllText($"/localdata/error_{Process.GetCurrentProcess().Id}.txt", ex.ToString());
+
+            //    Console.WriteLine(ex);
+            //}
+
+            //CreateWebHostBuilder(args).Build().StartAsync();
+            //var tcs = new TaskCompletionSource<bool>();
+            //tcs.Task.Wait();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
